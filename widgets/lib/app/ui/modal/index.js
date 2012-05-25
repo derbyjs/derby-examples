c5ab39cacb641803e99f773e18715adfee6ad362
nateps <nate@nateps.com>
2012-05-25 13:37:46 -0700

    close modal on escape

--- widgets/lib/app/ui/modal/index.js
+++ widgets/lib/app/ui/modal/index.js
@@ -1,11 +1,18 @@
-exports.create = function(self) {
+exports.create = function(self, dom) {
+  function close(button) {
+    var cancelled = self.trigger('close', button)
+    if (cancelled) return
+    self.set('show', false)
+  }
+
+  dom.addListener(document, 'keydown', function(e) {
+    if (e.keyCode === 27) {  // Escape
+      close('escape')
+    }
+  })
 
   exports.click = function(e) {
     var button = e.target.getAttribute('data-button')
-      , cancelled
-    if (!button) return
-    cancelled = self.trigger('close', button)
-    if (cancelled) return
-    self.set('show', false)
+    if (button) close(button)
   }
 }
