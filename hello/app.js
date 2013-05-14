8600dd57940a29c2388e64b4e5b76cf71528dc8e
Nate Smith <nate@nateps.com>
2013-05-13 22:19:52 -0700

    update hello example to new apis

---
+++ hello/app.js
@@ -0,0 +1,15 @@
+var app = require('derby').createApp(module);
+
+// Templates define both HTML and model <- -> view bindings
+app.view.make('Body',
+  'Holler: <input value="{hello.message}">' +
+  '<h2>{hello.message}</h2>'
+);
+
+// Routes render on client as well as server
+app.get('/', function(page, model) {
+  // Subscribe specifies the data to sync
+  model.subscribe('hello.message', function() {
+    page.render();
+  });
+});
