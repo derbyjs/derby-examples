bed0d911310a1bff5dba199bc757183471cda837
Nate Smith <nate@nateps.com>
2013-05-15 12:32:39 -0700

    use a query instead of maintaining ids in directory

--- directory/lib/app/index.js
+++ directory/lib/app/index.js
@@ -3,7 +3,7 @@ var derby = require('derby')
 derby.use(require('derby-ui-boot'))
 derby.use(require('../../ui'))
 
-var app = derby.createApp(module);
+var app = derby.createApp(module)
 
 var pages = [
   {url: '/', title: 'Home'}
@@ -24,9 +24,10 @@ app.get('/', function(page, model) {
 })
 
 app.get('/people', function(page, model, params, next) {
-  model.subscribe('people', 'directory.ids', function(err) {
+  var peopleQuery = model.query('people', {})
+  peopleQuery.subscribe(function(err) {
     if (err) return next(err)
-    model.refList('_page.people', 'people', 'directory.ids')
+    peopleQuery.ref('_page.people');
     render('people', page)
   })
 })
@@ -36,7 +37,7 @@ app.get('/people/:id', function(page, model, params, next) {
     return render('edit', page)
   }
   var person = model.at('people.' + params.id)
-  model.subscribe(person, 'directory.ids', function(err) {
+  model.subscribe(person, function(err) {
     if (err) return next(err)
     if (!person.get()) return next()
     model.ref('_page.person', person)
@@ -47,21 +48,20 @@ app.get('/people/:id', function(page, model, params, next) {
 app.fn({
   done: function() {
     var model = this.model;
-    var name = model.at('_page.person.name')
-    if (!name.get()) {
-      var checkName = name.on('change', function(value) {
+    var person = model.at('_page.person')
+    if (!person.get('name')) {
+      var checkName = person.on('change', 'name', function(value) {
         if (!value) return
         model.del('_page.nameError')
-        name.removeListener('change', checkName)
+        model.removeListener('change', checkName)
       })
       model.set('_page.nameError', true)
       document.getElementById('name').focus()
       return
     }
 
-    if (!model.get('_page.person.id')) {
-      var id = model.add('people', model.get('_page.person'))
-      model.push('directory.ids', id)
+    if (!person.get('id')) {
+      model.add('people', person.get())
     }
     app.history.push('/people')
   }
@@ -71,13 +71,8 @@ app.fn({
   }
 
 , deletePerson: function() {
-    var model = this.model;
-    var ids = model.at('directory.ids')
-    var id = model.get('_page.person.id')
-    var i = (ids.get() || []).indexOf(id)
-    if (i > -1) model.remove('directory.ids', i)
     // Update model without emitting events so that the page doesn't update
-    model.silent().del('_page.person')
+    this.model.silent().del('_page.person')
     app.history.back()
   }
-});
+})
