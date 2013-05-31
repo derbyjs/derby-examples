66835041c5452ec0ed31c231609ea8b631364060
Joseph Gentle <josephg@gmail.com>
2013-05-31 18:01:24 -0400

    Added a logger with timing info

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var express = require('express');
+var fs = require('fs');
 require('coffee-script');
 
 if (process.env.NODETIME_ACCOUNT_KEY) {
@@ -12,7 +13,15 @@ if (process.env.NODETIME_ACCOUNT_KEY) {
 var expressApp = express();
 var server = http.createServer(expressApp);
 
+var port = process.env.PORT || 3000;
+express.logger.token('port', function(req, res) { return port; });
+
 expressApp
+  .use('/_check', function(req, res) { res.send('OK'); })
+  .use(express.logger({
+    format: ':port :remote-addr - - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":user-agent" - :response-time ms',
+    stream: fs.createWriteStream('reqlog.txt', {flags:'a', encoding:'utf8', mode:0666})
+  }))
   .use(express.vhost('chat.derbyjs.com', require('./chat')))
   .use(express.vhost('directory.derbyjs.com', require('./directory')))
   .use(express.vhost('hello.derbyjs.com', require('./hello')))
@@ -20,7 +29,6 @@ expressApp
   .use(express.vhost('todos.derbyjs.com', require('./todos')))
   .use(express.vhost('widgets.derbyjs.com', require('./widgets')))
 
-var port = process.env.PORT || 3000;
 server.listen(port, function() {
   console.log('Go to: http://localhost:%d/', port);
 });
