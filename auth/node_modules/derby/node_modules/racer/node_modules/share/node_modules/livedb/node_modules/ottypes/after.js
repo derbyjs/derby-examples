13f87897d50b32cb897e9502a9fe0588578485d1
Nate Smith <nate@nateps.com>
2014-02-20 12:34:38 -0800

    remove auth example for now

--- auth/node_modules/derby/node_modules/racer/node_modules/share/node_modules/livedb/node_modules/ottypes/after.js
+++
@@ -1,9 +0,0 @@
-// This is included after the JS for each type when we build for the web.
-
-  var _types = window.ottypes = window.ottypes || {};
-  var _t = module.exports;
-  _types[_t.name] = _t;
-
-  if (_t.uri) _types[_t.uri] = _t;
-})();
-
