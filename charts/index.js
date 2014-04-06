888f3236c064208cdd19fdbb0ae0df999182a75c
Ian Johnson <enjalot@gmail.com>
2014-04-06 16:43:22 -0500

    demo the barchart components

---
+++ charts/index.js
@@ -0,0 +1,31 @@
+var app = module.exports = require('derby').createApp('charts', __filename);
+app.use(require('d-bootstrap'));
+app.loadViews(__dirname);
+app.loadStyles(__dirname);
+app.component(require('d-connection-alert'));
+app.component(require('d-barchart'));
+app.component(require('d-barchart-vanilla'));
+app.component(require('d-d3-barchart'));
+
+// This lets us play with the model in the console:
+//   MODEL.get("widgets");
+app.proto.create = function(model) {
+  global.MODEL = model
+}
+
+app.get('/', function(page, model, params, next) {
+  var data = model.at('widgets.data');
+  data.subscribe(function(err) {
+    if (err) return next(err);
+    data.setNull('foo', [{value: 1}, {value: 10}, {value: 20 }]);
+    page.render();
+  });
+});
+
+// adding a prototype method to page of app
+app.proto.remove = function(d,i,el) {
+  this.model.remove("widgets.data.foo", i, 1);
+}
+app.proto.add = function() {
+  this.model.push("widgets.data.foo", {value: Math.random() * 100 });
+}
