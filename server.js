9bb5456bdbf7e78062c81dcf02bdbe5b191b8b68
Ian Johnson <enjalot@gmail.com>
2014-07-14 15:28:16 -0700

    fix redis ports

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ require('coffee-script/register');
 if(process.env.MONGO_PORT_27017_TCP_ADDR != void 0 && process.env.MONGO_PORT_27017_TCP_PORT != void 0) {
   process.env.MONGO_URL = 'mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/';
 }
-if(process.env.REDIS_PORT_27017_TCP_ADDR != void 0 && process.env.REDIS_PORT_27017_TCP_PORT != void 0) {
-  process.env.REDIS_HOST = process.env.REDIS_PORT_27017_TCP_ADDR;
-  process.env.REDIS_PORT = process.env.MONGO_PORT_27017_TCP_PORT;
+if(process.env.REDIS_PORT_6379_TCP_ADDR != void 0 && process.env.REDIS_PORT_6379_TCP_PORT != void 0) {
+  process.env.REDIS_HOST = process.env.REDIS_PORT_6379_TCP_ADDR;
+  process.env.REDIS_PORT = process.env.MONGO_PORT_6379_TCP_PORT;
 }
 
 var derbyStarter = require('derby-starter/lib/server');
