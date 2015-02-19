dfe475c6d3af00bdb3e7f07d8c938759f3156f92
Will Riley <will@hflw.org>
2015-02-18 18:53:03 -0800

    added some comments relating to the bugs I fixed because I'm a nice person

--- directory/index.js
+++ directory/index.js
@@ -64,6 +64,8 @@ EditForm.prototype.done = function() {
 
   if (!model.get('person.id')) {
     model.root.add('people', model.get('person'));
+    // Wait for all model changes to go through before going to the next page, mainly because
+    // in non-single-page-app mode (basically IE < 10) we want changes to save to the server before leaving the page
     model.whenNothingPending(function(){
       app.history.push('/people');
     });
@@ -80,6 +82,8 @@ EditForm.prototype.cancel = function() {
 EditForm.prototype.deletePerson = function() {
   // Update model without emitting events so that the page doesn't update
   this.model.silent().del('person');
+  // Wait for all model changes to go through before going back, mainly because
+  // in non-single-page-app mode (basically IE < 10) we want changes to save to the server before leaving the page
   this.model.whenNothingPending(function(){
     app.history.back();
   });
