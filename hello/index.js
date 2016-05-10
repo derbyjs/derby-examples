f07b26f4447861200470ff1f124445da3fbd8720
ovvn <omazinov@gmail.com>
2016-05-10 18:15:18 +0300

    Fix 'hello' example.

--- hello/index.js
+++ hello/index.js
@@ -3,8 +3,15 @@ app.loadViews(__dirname);
 
 // Routes render on client as well as server
 app.get('/', function(page, model) {
+  var id = model.id();
+  var $messages = model.at('messages.' + id);
   // Subscribe specifies the data to sync
-  model.subscribe('hello.message', function() {
+  $messages.subscribe(function(err) {
+    if (err) return next(err);
+    var messages = $messages.get();
+    // If the message doesn't exist yet, we need to create it
+    if (!messages) model.add('messages', {id: id, text: ''});
+    $messages.ref('_page.hello');
     page.render();
   });
 });
