0d5c5efca259994ae04e60d5951f1b9a89d06460
Nate Smith <nate@nateps.com>
2018-07-07 21:12:27 -0700

    update use of d-codemirror to latest version

--- codemirror/server.js
+++ codemirror/server.js
@@ -1,11 +1,9 @@
-// In an actual app you may want to npm install codemirror (or use bower)
-var node_modules = __dirname + '/../node_modules/';
-var cm = node_modules + "d-codemirror/node_modules/codemirror/";
+var codemirror = require('d-codemirror');
 
 var options = {
   port: 8003,
   static: [
-    {route: '/cm', dir: cm },
+    {route: '/codemirror', dir: codemirror.root}
   ]
 };
 require('derby-starter').run(__dirname, options);
