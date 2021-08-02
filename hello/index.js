40cc631f9f533eec2fb21ba62d6842887d74dc05
Osman Mazinov <omazinov@gmail.com>
2021-08-02 17:44:58 +0300

    Fix 'hello' example

--- hello/index.js
+++ hello/index.js
@@ -2,7 +2,7 @@ var app = module.exports = require('derby').createApp('hello', __filename);
 app.loadViews(__dirname);
 
 // Routes render on client as well as server
-app.get('/', function(page, model) {
+app.get('/', function(page, model, params, next) {
   // Subscribe specifies the data to sync
   var message = model.at('hello.message');
   message.subscribe(function(err) {
